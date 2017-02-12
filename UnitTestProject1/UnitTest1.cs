1eebc66cb5d2cdee71d7d77b1fdddf0cd7384c0f
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-02-12 22:09:12 +0100

    Oprettet Test 1: Billig bil til under 80500,- fra før 2014

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -7,6 +7,15 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
-       
+        [TestMethod]
+        // Bil til under 80500,- fra før 2014 
+        public void TestMethod1()
+        {
+            Bil NyBilligBilRegistreringsafgift = new DieselBil("Ford", 75000, 2013, 18.9, "PG 97 357", true, 65);
+
+            double BilensRegistreringsAfgift = NyBilligBilRegistreringsafgift.RegistreringsAfgift();
+            Assert.AreEqual(78750, BilensRegistreringsAfgift);
+        }
+
     }
 }
